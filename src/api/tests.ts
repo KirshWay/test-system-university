f5e82f18009f4a3b2a9b682800bdfac21346e533
Victor <nemecvictor.kirsh@gmail.com>
2022-03-29 12:17:08 +0300

    update

--- src/api/tests.ts
+++ src/api/tests.ts
@@ -1,17 +1,17 @@
 import $axios from './index';
 
 const Tests = {
-  getAllTest: () => $axios.get(`${import.meta.env.VITE_SERVER}/test/list_test/`),
-  // getTest: (title: string) => $axios.get(`${import.meta.env.VITE_SERVER}/test/get_test/`, {title}),
-  createTest: (title: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/create_test/`, {title}),
-  updateTest: (title: string, testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/update_test/`, {title, testId}),
-  deleteTest: (testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/delete_test/`, {testId}),
-  createQuestion: (text: string, testId: string, typeTest: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/create_question/`, {text, testId, typeTest}),
-  updateQuestion: (text: string, testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/update_question/`, {text, testId}),
-  deleteQuestion: (text: string, testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/delete_question/`, {text, testId}),
-  createAnswer: (text: string, testId: string, correctAnswer: boolean) => $axios.post(`${import.meta.env.VITE_SERVER}/test/create_answer/`, {text, testId, correctAnswer}),
-  updateAnswerText: (text: string, testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/update_answer/`, {text, testId}),
-  deleteAnswer: (testId: string) => $axios.post(`${import.meta.env.VITE_SERVER}/test/delete_answer/`, {testId}),
+  getAllTest: () => $axios.get(`/test/list_test/`),
+  // getTest: (title: string) => $axios.get(`/test/get_test/`, {title}),
+  createTest: (title: string) => $axios.post(`/test/create_test/`, {title}),
+  updateTest: (title: string, testId: string) => $axios.post(`/test/update_test/`, {title, testId}),
+  deleteTest: (testId: string) => $axios.post(`/test/delete_test/`, {testId}),
+  createQuestion: (text: string, testId: string, typeTest: string) => $axios.post(`/test/create_question/`, {text, testId, typeTest}),
+  updateQuestion: (text: string, testId: string) => $axios.post(`/test/update_question/`, {text, testId}),
+  deleteQuestion: (text: string, testId: string) => $axios.post(`/test/delete_question/`, {text, testId}),
+  createAnswer: (text: string, testId: string, correctAnswer: boolean) => $axios.post(`/test/create_answer/`, {text, testId, correctAnswer}),
+  updateAnswerText: (text: string, testId: string) => $axios.post(`/test/update_answer/`, {text, testId}),
+  deleteAnswer: (testId: string) => $axios.post(`/test/delete_answer/`, {testId}),
 };
 
 export default Tests;
