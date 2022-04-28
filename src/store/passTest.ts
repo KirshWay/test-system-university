12c3fc101ed7e16246ebca8e43e1dc7572371424
Victor <nemecvictor.kirsh@gmail.com>
2022-04-28 17:20:28 +0300

    updates

--- src/store/passTest.ts
+++ src/store/passTest.ts
@@ -2,4 +2,4 @@ import {defineStore} from 'pinia';
 
 import {TestType} from '~/types/common';
 
-export const usePassTest = defineStore('usePassTest', {state: () => ({test: {} as TestType})});
+export const usePassTest = defineStore('usePassTest', {state: () => ({test: [] as TestType[]})});
